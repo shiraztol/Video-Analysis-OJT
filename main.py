8ea2223e3daf8435c9df4d239859d2cbb75ec04c
shiraztol <shirazto2000@gmail.com>
2022-12-21 18:12:20 +0200

    vms

--- main.py
+++ main.py
@@ -1,78 +1,43 @@
-from google.cloud import videointelligence
+import os
+from flask import Flask, abort, request, send_from_directory, jsonify, render_template, redirect, make_response, url_for, Response
+import sys
+from flask_cors import CORS, cross_origin
+import os
+import json
+import time
+import datetime
+execution_path = os.getcwd()
+
+app = Flask(__name__, template_folder=".")
+CORS(app)
+
+@app.route("/", methods= ['GET','POST'])
+@cross_origin()
+def homepage(): # Redirecting to home page
+    return render_template("index.html")
+
+@app.route("/get_json", methods= ['GET','POST']) # Receives loaded file name and requests JSON file 
+def get_json():
+    data = request.get_data('user_input')
+    data = str(data, 'utf-8')
+
+    return json_file
+
+@app.route("/<path:path>")
+@cross_origin()
+def static_dir(path):
+    file_name = request.get_data('json_input')
+    #print(f"This is the path: {path} and this is file_name {file_name}")
+    return send_from_directory(".", path)
 
 
-# This function is triggered when a file is added to a bucket
-# It will check if the file is a video, if it is, it will send it to the video intelligence API
-def file_added_to_bucket(event, context):
-    file_type = event["name"].split(".")[-1]
-    if file_type == "mp4":
-        procees_video( event, context)
+@app.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    else:
-        print("Not a video")
-        
+if __name__ == '__main__':
 
-
-
-# creating a client -- this is the video intelligence API
-video_client = videointelligence.VideoIntelligenceServiceClient()
-
-# This function will process the video and send it to the video intelligence API
-def procees_video( event, context):
-    # The name of the video file to analyze
-    file_to_analyze = f"gs://{event['bucket']}/{event['name']}"
-
-
-    # The path to the location of the results
-    # This will be a json file
-    output_path = "gs://results_ojt_video_analysis/{}.json".format(event['name'])
-
-    features = [ # This is the features that will be used to analyze the video
-    videointelligence.Feature.OBJECT_TRACKING,
-    videointelligence.Feature.LABEL_DETECTION,
-    videointelligence.Feature.SHOT_CHANGE_DETECTION,
-    videointelligence.Feature.SPEECH_TRANSCRIPTION,
-    videointelligence.Feature.LOGO_RECOGNITION,
-    videointelligence.Feature.EXPLICIT_CONTENT_DETECTION,
-    videointelligence.Feature.TEXT_DETECTION,
-    videointelligence.Feature.FACE_DETECTION,
-    videointelligence.Feature.PERSON_DETECTION
-    ]
-
-    # 1. Speech transcription
-    transcript_config = videointelligence.SpeechTranscriptionConfig(
-    language_code="en-US", enable_automatic_punctuation=True)
-
-    # 2. Person detection
-    person_config = videointelligence.PersonDetectionConfig(
-        include_bounding_boxes=True,
-        include_attributes=False,
-        include_pose_landmarks=True,)
-
-    # 3. Face detection
-    face_config = videointelligence.FaceDetectionConfig(
-        include_bounding_boxes=True, include_attributes=True)
-
-    # this is the video context, it will be used to analyze the video
-    # using the features that we have set (1,2,3)
-    video_context = videointelligence.VideoContext(
-        speech_transcription_config=transcript_config,
-        person_detection_config=person_config,
-        face_detection_config=face_config)
-
-    # This will send the video to the video intelligence API
-    # It will return an operation, which will be used to check the status of the video
-    operation = video_client.annotate_video(
-        request={"features": features,
-                "input_uri": file_to_analyze,
-                "output_uri": output_path,
-                "video_context": video_context}
-    )
-    # now we saved the output of the video to a json file
-
-    print("\nProcessing video.", operation)
-    
-
+    app.run(debug=True, host="0.0.0.0", port=8080)
 
 
 
